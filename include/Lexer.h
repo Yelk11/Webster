e01d47134d706940d454aa221a32ac0feec04c71
Yelk11 <neighbour.matthew@gmail.com>
2021-08-19 20:12:13 -0400

    rewriting it

---
+++ include/Lexer.h
@@ -0,0 +1,11 @@
+
+struct Lexer{
+    char* type;
+    char* 
+}lexer_t;
+
+void initLexer(char* input_str);
+
+
+
+
---
+++ src/Compiler.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "Lexer.h"
+
+int main(){
+    idk();
+    printf("Hello\n");
+    return 0;
+}
